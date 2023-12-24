b52943782f9c4d1d0f46df44618c6be641ab6f5a
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-24 17:50:32 +0530

    added some scripts

---
+++ geometrical_shapes.py
@@ -0,0 +1,89 @@
+import numpy as np
+import cv2
+from pathlib import Path
+from logger import logger
+
+# importing previous modules
+from intro import read_image, show_image
+
+
+
+def draw_rectangle(
+        img: np.ndarray, 
+        start_point: tuple, 
+        end_point:tuple, 
+        color:tuple,  
+        thickness: int) -> np.ndarray:
+    
+    """Draw a rectangle in the given image
+    
+    args:
+        img: numpy representation of image
+        start_point: starting point of rectangle
+        end_point: ending point of rectangle
+        color: RGB colors as a tuple eg:- (23,54,23)
+        thickness: the thickness of the rectangle
+        
+
+    return:
+        np.ndarray: numpy representation of new array        
+    """
+    image = cv2.rectangle(img, start_point, end_point, color, thickness)
+    logger.info(f"drawing a rectangle in the the image of color {color}")
+    return image
+
+
+def draw_line(
+        img: np.ndarray, 
+        start_point: tuple, 
+        end_point:tuple, 
+        color:tuple,  
+        thickness: int) -> np.ndarray:
+    
+    """Draw a line in the given image
+    
+    args:
+        img: numpy representation of image
+        start_point: starting point of line
+        end_point: ending point of line
+        color: RGB colors as a tuple eg:- (23,54,23)
+        thickness: the thickness of the rectangle
+        
+
+    return:
+        np.ndarray: numpy representation of new array        
+    """
+    image = cv2.line(img, start_point, end_point, color, thickness)
+    logger.info(f"drawing a line in the the image of color {color}")
+    return image
+
+
+def draw_circle(
+        img: np.ndarray,
+        center_cordinates: tuple,
+        radius: int,
+        color: tuple,
+        thickness: int ) -> np.ndarray:
+    """Draw a circle in the given image
+    
+    args:
+        img: numpy representation of image
+        center_cordinates: center coordinates of the circle
+        radius: radius of the circle
+        color: RGB colors as a tuple eg:- (23,54,23)
+        thickness: the thickness of the rectangle
+
+    return:
+        np.ndarray: numpy representation of new array        
+    """    
+    image = cv2.circle(img, center_cordinates, radius, color, thickness)
+    logger.info(f"drawing a circle of radius {radius} in the image")
+    return image
+
+
+if __name__ == "__main__":
+    img = read_image(Path("/home/izam/coding/opencv/data/cat.jpg"))
+    img_rectangle = draw_rectangle(img, (5, 5), (220, 220), (255, 0, 0), 2)
+    img_line = draw_line(img, (5, 5), (220, 220), (0, 255, 0), 2)
+    img_circle = draw_circle(img, (100, 100), 100, (0, 0, 255), 2)
+    show_image(name="cat", image=img_rectangle)
---
+++ intro.py
@@ -0,0 +1,59 @@
+import cv2
+from pathlib import Path
+import numpy as np
+from logger import logger
+
+def read_image(path: Path, flag: int = 1) -> np.ndarray:
+    """Reads a image
+    
+    Args:
+        path: the path to the image file
+        flag: the flag of the image
+
+    return:
+        pass
+    """
+    image = cv2.imread(str(path), flag)
+    logger.info(f"loaded the image {str(path)}")
+
+    return image
+
+
+def show_image(name: str, image: np.ndarray) -> None:
+    """show the cv2 image
+    
+    Args:
+        name: name of the image
+        image: numpy array representation of the image
+
+    return:
+        None
+    """
+    cv2.imshow(name, image)
+    logger.info(f"showing the image {name}")
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+def save_image(path: Path, image: np.ndarray) -> bool:
+    """Save the cv2 image
+    
+    args:
+        path: path to the image file
+        image: the numpy representation of the image
+    return: 
+        None
+    """
+    
+    saved = cv2.imwrite(str(path), image)
+    logger.info(f"save file {path} status : {saved}")
+    return saved
+
+
+if __name__ == "__main__":
+    path = Path("/home/izam/coding/opencv/data/cat.jpg")
+    image = read_image(path, flag=128)
+    show_image(name="cat", image=image)
+    save_image(Path("artifacts/new.jpg"), image)
